d02becc6475c3623e567efa64be3a672a4ea9482
Ashu16n <ashutoshupadhyay18@gmail.com>
2024-06-09 17:12:30 +0530

    updated function name

---
+++ src/test/java/testCaseFile/Jenkin2NG.java
@@ -0,0 +1,12 @@
+package testCaseFile;
+
+import org.testng.annotations.Test;
+
+public class Jenkin2NG {
+	@Test
+	void m1()
+	{
+		System.out.println("new jenkins server");
+	}
+
+}
---
+++ src/test/java/testCaseFile/Jenkn.java
@@ -0,0 +1,12 @@
+package testCaseFile;
+
+import org.testng.annotations.Test;
+
+public class Jenkn {
+
+	public static void main(String []args) {
+		System.out.println("jai mata di");
+
+	}
+
+}

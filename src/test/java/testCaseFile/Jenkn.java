1714e99be78075b24c296984baf8c9f888bea2f1
Ashu16n <167619027+Ashu16n@users.noreply.github.com>
2024-06-09 17:26:25 +0530

    message changed ok

--- src/test/java/testCaseFile/Jenkn.java
+++ src/test/java/testCaseFile/Jenkn.java
@@ -5,7 +5,8 @@ import org.testng.annotations.Test;
 public class Jenkn {
 
 	public static void main(String []args) {
-		System.out.println("jai mata di");
+		System.out.println("jai mata diiii");
+		
 
 	}
 
